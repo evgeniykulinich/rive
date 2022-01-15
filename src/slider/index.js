eb2a458ba77bd9bee0f25bbb06199f80573e25f7
Evgeniy <evgeniy.kulinich23@gmail.com>
2022-01-15 18:57:31 +0300

    change src path and make biggest slider arrows

--- src/slider/index.js
+++ src/slider/index.js
@@ -1,23 +1,29 @@
 import Slider from "react-slick";
 import Rive from "rive-react";
 
+import AnimationMixing from "./rivFiles/animation_mixing.riv";
+import CharacterRigging from "./rivFiles/character_rigging.riv";
+import CloudRendering from "./rivFiles/cloud_rendering.riv";
+import InverseKinematics from "./rivFiles/inverse_kinematics.riv";
+import RealtimeCollaboration from "./rivFiles/realtime_collaboration.riv";
+import TinyFileSizes from "./rivFiles/tiny_file_sizes.riv";
+
 export const RiveSlider = () => {
   const settings = {
+    centerMode: true,
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
   };
   return (
     <div className="container">
       <Slider {...settings}>
-        <Rive src="1_-_team.riv" />
-        <Rive src="2_-_files.riv" />
-        <Rive src="5_-_knight.riv" />
-        <Rive src="example__tom_morello.riv" />
-        <Rive src="new_file.riv" />
-        <Rive src="skills_demo.riv" />
+        <Rive src={AnimationMixing} />
+        <Rive src={CharacterRigging} />
+        <Rive src={CloudRendering} />
+        <Rive src={InverseKinematics} />
+        <Rive src={RealtimeCollaboration} />
+        <Rive src={TinyFileSizes} />
       </Slider>
     </div>
   );
